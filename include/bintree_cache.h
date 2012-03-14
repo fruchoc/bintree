20450b9d9df4d08b08f3e8117922baac0dec0249
William Menz <wjm34@cam.ac.uk>
2012-03-14 15:54:19 +0000

    Initial add of binary cache files.

---
+++ include/bintree_cache.h
@@ -0,0 +1,20 @@
+/*
+ * bintree_cache.h
+ *
+ *  Created on: 14 Mar 2012
+ *      Author: wjm34
+ */
+
+#ifndef BINTREE_CACHE_H_
+#define BINTREE_CACHE_H_
+
+class BintreeCache {
+public:
+    //! Default constructor
+    BintreeCache();
+
+    //! Default destructor
+    virtual ~BintreeCache();
+};
+
+#endif /* BINTREE_CACHE_H_ */
---
+++ src/bintree_cache.cpp
@@ -0,0 +1,17 @@
+/*
+ * bintree_cache.cpp
+ *
+ *  Created on: 14 Mar 2012
+ *      Author: wjm34
+ */
+
+#include "../include/bintree_cache.h"
+
+BintreeCache::BintreeCache() {
+    // TODO Auto-generated constructor stub
+
+}
+
+BintreeCache::~BintreeCache() {
+    // TODO Auto-generated destructor stub
+}
