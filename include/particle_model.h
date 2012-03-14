adab1f8e647be9684f01a0379a2b70d9a1a6f071
William Menz <wjm34@cam.ac.uk>
2012-03-14 15:04:17 +0000

    Change tabs to spaces.

--- include/particle_model.h
+++ include/particle_model.h
@@ -17,49 +17,49 @@ using namespace std;
 
 class ParticleModel {
 public:
-	//! Default constructor
-	ParticleModel();
+    //! Default constructor
+    ParticleModel();
 
-	//! Default destructor
-	virtual ~ParticleModel(void);
+    //! Default destructor
+    virtual ~ParticleModel(void);
 
-	//! Print the particle state space
-	void PrintParticle() const;
+    //! Print the particle state space
+    void PrintParticle() const;
 
-	// SERIALISATION OF OBJECT
-	//! Write object to output stream
-	void Serialise(ostream &out) const;
+    // SERIALISATION OF OBJECT
+    //! Write object to output stream
+    void Serialise(ostream &out) const;
 
-	//! Write state space to stream
-	void SerialisePrimary(ostream &out) const;
+    //! Write state space to stream
+    void SerialisePrimary(ostream &out) const;
 
-	//! Read object from stream
-	void Deserialise(istream &in);
+    //! Read object from stream
+    void Deserialise(istream &in);
 
-	//! Read state space from stream
-	void DeserialisePrimary(istream &in);
+    //! Read state space from stream
+    void DeserialisePrimary(istream &in);
 
 private:
-	//! State of particle property 1
-	int m_prop1;
+    //! State of particle property 1
+    int m_prop1;
 
-	//! State of particle property 2
-	int m_prop2;
+    //! State of particle property 2
+    int m_prop2;
 
-	//! Pointer to leftchild
-	ParticleModel *m_leftchild;
+    //! Pointer to leftchild
+    ParticleModel *m_leftchild;
 
-	//! Pointer to rightchild
-	ParticleModel *m_rightchild;
+    //! Pointer to rightchild
+    ParticleModel *m_rightchild;
 
-	//! Pointer to leftparticle
-	ParticleModel *m_leftparticle;
+    //! Pointer to leftparticle
+    ParticleModel *m_leftparticle;
 
-	//! Pointer to rightparticle
-	ParticleModel *m_rightparticle;
+    //! Pointer to rightparticle
+    ParticleModel *m_rightparticle;
 
-	//! Pointer to parent
-	ParticleModel *m_parent;
+    //! Pointer to parent
+    ParticleModel *m_parent;
 };
 
 #endif /* PARTICLE_MODEL_H_ */
--- src/particle_model.cpp
+++ src/particle_model.cpp
@@ -14,100 +14,100 @@
 using namespace std;
 
 /*!
- * @brief 		Initialising constructor.
+ * @brief       Initialising constructor.
  */
 ParticleModel::ParticleModel() :
-	// State space
-	m_prop1(0),
-	m_prop2(0),
-	// Children
-	m_leftchild(NULL),
-	m_rightchild(NULL),
-	// Particles
-	m_leftparticle(NULL),
-	m_rightparticle(NULL),
-	// Parent
-	m_parent(NULL)
+    // State space
+    m_prop1(0),
+    m_prop2(0),
+    // Children
+    m_leftchild(NULL),
+    m_rightchild(NULL),
+    // Particles
+    m_leftparticle(NULL),
+    m_rightparticle(NULL),
+    // Parent
+    m_parent(NULL)
 {}
 
 /*!
- * @brief		Default destructor.
+ * @brief       Default destructor.
  */
 ParticleModel::~ParticleModel()
 {
-	delete m_leftchild;
-	delete m_rightchild;
+    delete m_leftchild;
+    delete m_rightchild;
 }
 
 /*!
- * @brief		Writes the state of the particle to console.
+ * @brief       Writes the state of the particle to console.
  */
 void ParticleModel::PrintParticle() const
 {
-	cout << this << " p1: " << m_prop1 << " p2: " << m_prop2;
-	cout << " lc: " << m_leftchild << " rc: " << m_rightchild;
-	cout << " lp: " << m_leftparticle << " rp: " << m_rightparticle;
-	cout << " parent: " << m_parent << endl;
+    cout << this << " p1: " << m_prop1 << " p2: " << m_prop2;
+    cout << " lc: " << m_leftchild << " rc: " << m_rightchild;
+    cout << " lp: " << m_leftparticle << " rp: " << m_rightparticle;
+    cout << " parent: " << m_parent << endl;
 }
 
 /*!
- * @brief		Writes the object to a binary stream
- * @param 	out	Output binary stream
+ * @brief           Writes the object to a binary stream
+ * @param   out     Output binary stream
  */
 void ParticleModel::Serialise(ostream &out) const
 {
-	if (out.good()) {
-		// Write version
-		const unsigned int version = 0;
-		out.write((char*)version, sizeof(version));
-	}
+    if (out.good()) {
+        // Write version
+        const unsigned int version = 0;
+        out.write((char*)version, sizeof(version));
+    }
 }
 
 /*!
- * @brief		Writes the state space to a binary stream
- * @param 	out	Output binary stream
+ * @brief           Writes the state space to a binary stream
+ * @param   out     Output binary stream
  */
 void ParticleModel::SerialisePrimary(ostream &out) const
 {
-	if (out.good()) {
-		// Write state space
-		int val = 0;
-		val = m_prop1;
-		out.write((char*)val, sizeof(val));
+    if (out.good()) {
+        // Write state space
+        int val = 0;
+        val = m_prop1;
+        out.write((char*)val, sizeof(val));
 
-		val = m_prop2;
-		out.write((char*)val, sizeof(val));
-	}
+        val = m_prop2;
+        out.write((char*)val, sizeof(val));
+    }
 }
 
 
 /*!
- * @brief		Reads the object from a binary stream
- * @param 	in	Input binary stream
+ * @brief           Reads the object from a binary stream
+ * @param   in      Input binary stream
  */
 void ParticleModel::Deserialise(istream &in)
 {
-	if (in.good()) {
-		// Read version
-		unsigned int version = 0;
-		in.read(reinterpret_cast<char*>(&version), sizeof(version));
-	}
+    if (in.good()) {
+        // Read version
+        unsigned int version = 0;
+        in.read(reinterpret_cast<char*>(&version), sizeof(version));
+    }
 }
 
 
 /*!
- * @brief		Reads the state space from a binary stream
- * @param 	in	Input binary stream
+ * @brief           Reads the state space from a binary stream
+ * @param   in      Input binary stream
  */
 void ParticleModel::DeserialisePrimary(istream &in)
 {
-	if (in.good()) {
-		// Read state space
-		int val = 0;
-		in.read(reinterpret_cast<char*>(&val), sizeof(val));
-		m_prop1 = (int)val;
+    if (in.good()) {
+        // Read state space
+        int val = 0;
+        in.read(reinterpret_cast<char*>(&val), sizeof(val));
+        m_prop1 = (int)val;
 
-		in.read(reinterpret_cast<char*>(&val), sizeof(val));
-		m_prop2 = (int)val;
-	}
+        in.read(reinterpret_cast<char*>(&val), sizeof(val));
+        m_prop2 = (int)val;
+    }
 }
